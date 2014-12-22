c73f4f674d5bc5c35566482c819a3c894c00e15f
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-22 12:28:52 +0000

    adding dish class

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,17 @@
 class Dish
+
+  DISHES = {fish: 10.5, fries: 2.5, soup: 3.4}
+
+  attr_reader :name,:price
+
+  def initialize(name,price)
+    @name = name
+    @price = price
+  end
+
+  def self.method_missing name,*args
+    return new name,DISHES[name] if DISHES[name]
+    super
+  end
+
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,7 +2,14 @@ require 'dish'
 
 describe 'dish' do 
 
+  dish = Dish.new('salmon',15.3)
+
   it 'should be able to create a dish' do
+    expect(dish.name).to eq('salmon')
   end  
+
+  it 'should create a fish dish' do
+    expect(Dish.fish.price).to eq(10.5)
+  end
   
 end
