41d20c6294de0e4782f246a8004208bb56470abc
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-23 09:14:29 +0000

    adding additional order

--- lib/order.rb
+++ lib/order.rb
@@ -9,4 +9,8 @@ class Order
     @order.merge!(positions)
     @order.merge!({id: id})
   end 
+  #adding new order to existing one
+  def add_new(positions)
+    @order.merge!(positions)
+  end
 end
