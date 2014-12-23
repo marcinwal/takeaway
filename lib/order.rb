b8ee7181a2187c0f0cf55a1fbff6845b660a21f1
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-23 09:09:58 +0000

    order initialize

--- lib/order.rb
+++ lib/order.rb
@@ -1,2 +1,11 @@
+require 'byebug'
+
 class Order
+  attr_reader :order
+
+  def initialize(positions)
+    #byebug
+    @order = {}
+    @order.merge!(positions)
+  end 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,7 +2,8 @@ require 'order'
 
 describe 'order to restaurant' do
 
-  let(:order){Order.new}
+  let(:order){Order.new({fish: 2,coffee: 2})}
   it 'should send the order to the restaurant'do
+    expect(order.order[:fish]).to eq(2)
   end  
 end
