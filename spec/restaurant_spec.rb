dc2c0b0be3bc8729533ba4d9da8de29705316d1b
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-25 11:00:48 +0000

    raising errors if sum is wrong

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -5,6 +5,7 @@ describe 'restaurant' do
   let(:restaurant){Restaurant.new}
   let(:user){double :user}
   let(:order){double :order,items_sum: 2}
+  let(:order_2){double :order_2,items_sum: 3}
   it 'should have some dishes initially' do
     expect(restaurant.check_menu(:fish)).to eq(20)
   end
@@ -13,6 +14,9 @@ describe 'restaurant' do
     restaurant.order(user,order,2)
   end  
 
+  it 'should raise the error if items do not match' do
+    expect {restaurant.order(user,order_2,2)}.to raise_error(RuntimeError,"Wrong check sum ")
+  end
 
 end
 
